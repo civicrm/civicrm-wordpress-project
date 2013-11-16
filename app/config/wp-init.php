567cc37425c787bdfb59ca44bf79bc34f87c607d
Tim Otten <totten@civicrm.org>
2013-11-16 01:13:17 -0800

    mk-wp-demo-site - Consolidate some code (for the moment)

--- app/config/wp-init.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Initialize the instance of WordPress
-
-// Use a static front page
-$home = get_page_by_title('Welcome to CiviCRM with WordPress');
-update_option('page_on_front', $home->ID);
-update_option('show_on_front', 'page');
