4a7b4ee4056a7a519c49200970316710972729fc
Tim Otten <totten@civicrm.org>
2013-11-16 01:07:22 -0800

    civi-wp-startup - Add experimental script for running Civi+WP in PHP's standalone web server

---
+++ src/wp-router.php
@@ -0,0 +1,21 @@
+<?php
+// SOURCE: http://ripeworks.com/run-wordpress-locally-using-phps-buily-in-web-server/
+$root = $_SERVER['DOCUMENT_ROOT'];
+chdir($root);
+$path = '/' . ltrim(parse_url($_SERVER['REQUEST_URI'])['path'], '/');
+set_include_path(get_include_path() . ':' . __DIR__);
+if (file_exists($root . $path)) {
+  if (is_dir($root . $path) && substr($path, strlen($path) - 1, 1) !== '/') {
+    $path = rtrim($path, '/') . '/index.php';
+  }
+  if (strpos($path, '.php') === FALSE) {
+    return FALSE;
+  }
+  else {
+    chdir(dirname($root . $path));
+    require_once $root . $path;
+  }
+}
+else {
+  include_once 'index.php';
+}
